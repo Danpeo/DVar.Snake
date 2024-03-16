011269a547155912f1514446009b9e6c63a645c2
Danpeo <danilvar4@gmail.com>
2024-03-17 02:29:26 +0400

    fin

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ brightred_clr = rl.Color(239, 35, 60, 255)
 
 cell_size = 30
 cell_count = 25
+offset = 75
 
 def is_in_deque(element: rl.Vector2, deque: deque[Vector2]) -> bool:
     for i in deque:
@@ -43,6 +44,8 @@ class Game:
         self.snake.update()
         self.check_snake_collision_with_food()
         self.check_snake_collision_with_bounds()
+        self.check_snake_collision_with_tail()
+        
         
     def check_snake_collision_with_food(self):
         if rl.vector2_equals(self.snake.body[0], self.food.position):
@@ -53,10 +56,19 @@ class Game:
         if self.snake.body[0].x < 0 or self.snake.body[0].x >= cell_count or self.snake.body[0].y < 0 or self.snake.body[0].y >= cell_count:
             self.game_over()
             
+    def check_snake_collision_with_tail(self):
+        for i in range(1, len(self.snake.body)):
+            if rl.vector2_equals(self.snake.body[0], self.snake.body[i]):
+                self.game_over()
+                break
+
+            
     def game_over(self):
         self.snake.initialize()
         self.food.position = self.food.generate_random_pos(self.snake.body)
         self.is_running = False
+        
+
         
 class Snake:
     def __init__(self) -> None:
@@ -67,7 +79,7 @@ class Snake:
         for i in range(len(self.body)):
             x = self.body[i].x
             y = self.body[i].y
-            rl.draw_rectangle(x * cell_size, y * cell_size, cell_size, cell_size, black_clr)
+            rl.draw_rectangle(offset + x * cell_size, offset + y * cell_size, cell_size, cell_size, black_clr)
             
     def update(self):
         self.body.appendleft(rl.vector2_add(self.body[0], self.direction))
@@ -77,20 +89,17 @@ class Snake:
             self.body.pop()
             
     def initialize(self):
-        self.body = self.set_init_pos()
+        self.body = deque([Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)])
         self.direction = Vector2(1, 0)
     
-    def set_init_pos(self):
-        x = cell_count/2
-        y = cell_count/2
-        return deque([Vector2(x, y), Vector2(x-1, y), Vector2(x-2, y)])
+
 
 class Food:
     def __init__(self, snakeBody: deque[Vector2]):
         self.position = self.generate_random_pos(snakeBody)
         
     def draw(self):
-        body = rl.Rectangle(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
+        body = rl.Rectangle(offset + self.position.x * cell_size, offset + self.position.y * cell_size, cell_size, cell_size)
         rl.draw_rectangle_rounded(body, 0.5, 1, brightred_clr)
         
     def generate_random_pos(self, snakeBody: deque[Vector2]) -> rl.Vector2:
@@ -105,7 +114,7 @@ class Food:
         return pos
 
 def main():
-    screen_size = cell_size * cell_count
+    screen_size = 2 * offset + cell_size * cell_count
     rl.init_window(screen_size, screen_size, "SNAKE THE GAME")
     rl.set_target_fps(60)
     
@@ -136,7 +145,9 @@ def main():
         
         
         rl.clear_background(yellow_clr)
-        
+        rl.draw_rectangle_lines_ex(rl.Rectangle(offset - 5, offset - 5, cell_size * cell_count + 10, cell_size * cell_count + 10), 5, black_clr)
+        rl.draw_text("SNAKE", offset - 5, 20, 40, brightred_clr);
+
         game.draw()
         
         rl.end_drawing()
